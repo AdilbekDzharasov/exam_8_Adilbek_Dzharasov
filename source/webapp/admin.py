ef866cb04bff0cb554c709f513ab357223b2b307
Адильбек Джарасов <dzharasov97@gmail.com>
2022-10-29 12:07:49 +0600

    Создал модели продукта и отзыва, прописал админку, провел миграции, через админку создал тестовые продукт и отзыв.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from webapp.models import Product, Review
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'description', 'image', 'is_deleted']
+    list_filter = ['id', 'name', 'category', 'description', 'image', 'is_deleted']
+    search_fields = ['id', 'name', 'category', 'description']
+    fields = ['name', 'category', 'description', 'image', 'is_deleted']
+    readonly_fields = ['id']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'product', 'review_text', 'grade', 'is_deleted']
+    list_filter = ['id', 'author', 'product', 'review_text', 'grade', 'is_deleted']
+    search_fields = ['id', 'author', 'product']
+    fields = ['author', 'product', 'review_text', 'grade', 'is_deleted']
+    readonly_fields = ['id']
+
+
+admin.site.register(Review, ReviewAdmin)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,114 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('other', 'Other'),
+        ('sport', 'Sport'),
+        ('computers', 'Computers')
+    ]
+
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="Name"
+    )
+    category = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        choices=CATEGORY_CHOICES,
+        default=CATEGORY_CHOICES[0][0],
+        verbose_name='Category'
+    )
+    description = models.TextField(
+        max_length=3000,
+        null=True,
+        blank=True,
+        verbose_name="Description"
+    )
+    image = models.ImageField(
+        upload_to="images",
+        null=True,
+        blank=True,
+        verbose_name="image"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Date of creation'
+    )
+    changed_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Date of change'
+    )
+    is_deleted = models.BooleanField(
+        verbose_name='Is deleted',
+        default=False,
+        null=False
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+class Review(models.Model):
+    DEFAULT_CHOICES = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    ]
+
+    author = models.ForeignKey(
+        to=User,
+        related_name='reviews',
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name='Author'
+    )
+    product = models.ForeignKey(
+        to="webapp.Product",
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Product"
+    )
+    review_text = models.TextField(
+        max_length=5000,
+        null=False,
+        blank=False,
+        verbose_name="Review text"
+    )
+    grade = models.IntegerField(
+        null=False,
+        blank=False,
+        choices=DEFAULT_CHOICES,
+        verbose_name="Grade"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Date of creation'
+    )
+    changed_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Date of change'
+    )
+    is_deleted = models.BooleanField(
+        verbose_name='Is deleted',
+        default=False,
+        null=False
+    )
+
+    def __str__(self):
+        return f"{self.author}. {self.product}. {self.review_text}. {self.grade}"
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+
